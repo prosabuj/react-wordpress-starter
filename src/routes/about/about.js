bd3ebef9fabac03155a0abb40d6875e7c075caae
Sabuj Ali <prosabuj98@gmail.com>
2019-10-18 10:20:29 +0600

    first commit

---
+++ src/routes/about/about.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="container">
+      <div className="position-relative overflow-hidden text-center">
+        <div className="col-md-8 p-lg-5 mx-auto my-5">
+          <h1 className="display-4 font-weight-normal">Wordpress Blog</h1>
+          <p className="lead font-weight-normal">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim
+            ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+            aliquip ex ea commodo consequat.
+          </p>
+          <hr />
+          <h3 className="font-weight-normal">Version: 1.0.0</h3>
+          <p>Created by ♥ Sabuj Ali</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
